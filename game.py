8d7bb2ce96270e7fbe0fe60113d71a2acc94ec22
Glitch (superfestivus) <none>
2020-12-12 02:31:41 +0000

    💈🙅 Updated with Glitch

--- game.py
+++ game.py
@@ -1,27 +1,56 @@
-# Import and initialize the pygame library
+# Simple pygame program
+
+# Import 
 import pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+#initialize the pygame
 pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Run until the user asks to quit
 running = True
 while running:
-
-    # Did the user click the window close button?
+    # Look at every event in the queue
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
             running = False
 
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    # Create a surface and pass in a tuple containing its length and width
+    surf = pygame.Surface((50, 50))
 
-    # Flip the display
+    # Give the surface a color to separate it from the background
+    surf.fill((0, 0, 0))
+    rect = surf.get_rect()
+    screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
     pygame.display.flip()
-
 # Done! Time to quit.
 pygame.quit()
